2c4186a056a1ad6ce3ed3e3126f253d7d6e25ad6
ArnaavMahajan <62941728+ArnaavMahajan@users.noreply.github.com>
2020-07-02 16:03:20 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# C38-project
+# p5.play-boilerplate
+CLASS 20 PROJECT BOILERPLATE
---
+++ sketch.js
@@ -0,0 +1,20 @@
+var base,string,pendulum;
+function setup(){
+
+ createCanvas(1200,600);
+ pendulum=rect(600,300,40,40);
+
+string=rect(500,300,60,20);
+
+base=rect(400,200,10,90);
+
+}
+
+
+function draw(){
+background("white");
+
+
+
+drawSprites();
+}
